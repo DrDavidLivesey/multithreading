c72463f4dee6022326e3e6996719a003ad0bfc85
DrDavidLivesey <lapkin2001@inbox.ru>
2026-09-15 01:22:59 +0300

    Update logger.hpp

--- src/logger.hpp
+++ src/logger.hpp
@@ -1,26 +1,59 @@
 #pragma once
+
+#include <condition_variable>
 #include <format>
+#include <mutex>
+#include <queue>
+#include <string>
 #include <stop_token>
+#include <string_view>
 
 extern void (*print)(std::string_view message);
 
 class logger {
-public:
-  /// Called on many threads. Must queue the message.
-  /// Should return as soon as possible and rarely block.
-  void post(std::string_view message) {
-    print(message);  // TODO: Queue message instead.
-  }
+  private:
+    std::mutex mutex_;
+    std::condition_variable_any cv_;
+    std::queue<std::string> queue_;
 
-  /// Called on many threads. Must format and queue the message.
-  /// Should return as soon as possible and rarely block.
-  template <class... Args>
-  void post(std::format_string<Args...> fmt, Args&&... args) {
-    post(std::format(fmt, std::forward<Args>(args)...));
-  }
+  public:
+    void post(std::string_view message) {
+      {
+        std::lock_guard lock { mutex_ };
+        queue_.emplace(message);
+      }
 
-  /// Called on one thread. Must call `print` once per message.
-  void run(std::stop_token stop) {
-    while (!stop.stop_requested()) {}  // TODO: Print queued messages instead.
-  }
+      cv_.notify_one();
+    }
+
+    template <class... Args>
+    void post(std::format_string<Args...> fmt, Args&&... args) {
+      post(std::format(
+        fmt,
+        std::forward<Args>(args)...
+      ));
+    }
+
+    void run(std::stop_token stop) {
+      while (true) {
+        std::string message;
+
+        {
+          std::unique_lock lock { mutex_ };
+
+          cv_.wait(lock, stop, [&] {
+            return !queue_.empty();
+          });
+
+          if (queue_.empty() && stop.stop_requested()) {
+            return;
+          }
+
+          message = std::move(queue_.front());
+          queue_.pop();
+        }
+
+        print(message);
+      }
+    }
 };
